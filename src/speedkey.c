45f83dc0c99bced2a3cd8fe5266daf283230bfa0
Fernando Vezzosi <fv@linuxvar.it>
2010-03-18 22:17:23 +0100

    docs for -d parameter

--- src/speedkey.c
+++ src/speedkey.c
@@ -142,6 +142,7 @@ main (int argc , char * const argv[]) {
 				printf("Where OPTION is one of:\n");
 				printf("   --version,      -v     show the program's version\n");
 				printf("   --help,         -h     print this help message\n");
+				printf("   --debian,       -d     generate debian's /etc/network/interfaces format\n");
 				printf("   --year-start Y, -s Y   generate serials starting at the given year\n");
 				printf("   --year-end   Y, -s Y   generate serials up to the given year\n");
 				printf("   --threads    T, -t T   number of threads to use\n");
