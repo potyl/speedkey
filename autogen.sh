d899b6036ef7508fe8bfc17077c78e6aebdc3040
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2010-08-22 08:37:32 +0200

    Add compatibility with OS X

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-aclocal --force
+# In OS X the ports system installs the aclocal macro in a different location
+includes=
+if [ -e /opt/local/share/aclocal ]; then
+	includes="-I /opt/local/share/aclocal"
+fi
+
+aclocal $includes --force
 autoconf --force
 autoheader --force
 automake --add-missing --copy --force-missing
