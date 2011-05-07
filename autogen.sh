144b61216a7bd488b9ee6b8c627c82d8b6a0be40
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-05-07 12:01:32 +0200

    Add /usr/local/share/aclocal to the aclocal include path
    Closes issue #4 - doesn't compile on OS X
    Reported by hanekomu

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,13 @@
 # In OS X the ports system installs the aclocal macro in a different location
 includes=
 if [ -e /opt/local/share/aclocal ]; then
-	includes="-I /opt/local/share/aclocal"
+	includes="-I /opt/local/share/aclocal $includes"
 fi
+
+if [ -e /usr/local/share/aclocal ]; then
+	includes="-I /usr/local/share/aclocal $includes"
+fi
+
 
 aclocal $includes --force
 autoconf --force
